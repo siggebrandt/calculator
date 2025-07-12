7b0a7a1e0ccc66393a5944191e25555eab4f26c3
Sigge Brandt <sigge.brandt567@gmail.com>
2025-07-12 19:18:45 +0200

    layout testing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,8 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="bg-blue-200 text-center">this is the header</div>
+
+<div class="bg-pink-100">{@render children()}</div>
+
+<div class="bg-green-200 text-center">this is the footer</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
+<script>
+	function calculate() {}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <div class="bg-amber-300"></div>
 <div class="bg-red-700">hello</div>
+
+<input id="inoutField" />
+<br />
+<button on:click={(document.querySelector('input').value += ' dang')}>=</button>
