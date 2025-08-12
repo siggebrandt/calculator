9228df7e3cd70acf3d6c32ef84481d5a35a01d6b
Sigge Brandt <sigge.brandt567@gmail.com>
2025-08-12 12:21:14 +0200

    skapade knappar för kalkulatorn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,10 @@
 	let { children } = $props();
 </script>
 
-<div class="bg-blue-200 text-center">this is the header</div>
+<div class="bg-blue-200 text-center h-0.5/10">this is the header</div>
 
-<div class="bg-pink-100">{@render children()}</div>
+<div class="bg-gray-100 h-full">{@render children()}</div>
 
-<div class="bg-green-200 text-center">this is the footer, check out <a href="/about">ABOUT</a></div>
+<div class="bg-green-200 text-center h-0.5/10">
+	this is the footer, check out <a href="/about">ABOUT</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,62 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<div class="bg-amber-300"></div>
-<div class="bg-red-700"><a href="/about"> hello</a></div>
+<!-- <div class="bg-amber-300"></div> -->
+<!-- <div class="bg-red-700"><a href="/about"> hello</a></div> -->
 
-<input id="inoutField" />
-<br />
-<button on:click={(document.querySelector('input').value += ' dang')}>=</button>
+<input id="inoutField" class="bg-green-100 border" />
+<hr />
+
+<button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>9</button
+>
+<button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>8</button
+>
+<button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>7</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>6</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>5</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>4</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>3</button
+>
+<button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>2</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>1</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>0</button
+>
+<button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>+</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>-</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>×</button
+><button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>÷</button
+>
+<hr />
+<!-- här skickar vi till APIn för att calkulera-->
+<button
+	class="bg-pink-700 text-white px-5 ms-5"
+	on:click={(document.querySelector('input').value += this.textContent)}>=</button
+>
