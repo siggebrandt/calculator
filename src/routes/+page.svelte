f47df2a1fc51dc6f893d36ccdf38d5f9a58cd223
Sigge Brandt <sigge.brandt567@gmail.com>
2025-08-12 16:07:45 +0200

    skapade fetch funktion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,44 @@
 <script>
-	function calculate() {}
+	let inputtedExpression = ''; // synkas automatiskt med inputField
+
+	function addToExpression(input) {
+		inputtedExpression += input;
+	}
+	async function calculate() {
+		console.log(inputtedExpression);
+		const response = await fetch(`${document.URL}/api/calc`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ inputtedExpression })
+		});
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<!-- <div class="bg-amber-300"></div> -->
-<!-- <div class="bg-red-700"><a href="/about"> hello</a></div> -->
+<div class="text-right mr-5">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
 
-<input id="inoutField" class="bg-green-100 border" />
-<hr />
+<input bind:value={inputtedExpression} id="inputField" class="bg-green-100 border ml-5" />
+<button class="bg-pink-500 text-white px-5 ms-5 ml-5" on:click={calculate}>calc</button>
 
-<button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>9</button
->
-<button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>8</button
->
-<button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>7</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>6</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>5</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>4</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>3</button
->
-<button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>2</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>1</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>0</button
->
-<button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>+</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>-</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>×</button
-><button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>÷</button
->
-<hr />
-<!-- här skickar vi till APIn för att calkulera-->
-<button
-	class="bg-pink-700 text-white px-5 ms-5"
-	on:click={(document.querySelector('input').value += this.textContent)}>=</button
->
+<hr class="m-2" />
+
+<div id="calculatorButtons" class="text-center">
+	<!-- borde vara flex istället för center text-->
+	{#each [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] as buttonNumber}
+		<button class="bg-pink-700 text-white px-5 ms-5" on:click={() => addToExpression(buttonNumber)}
+			>{buttonNumber}</button
+		>
+	{/each}
+	<br class="mb-4" />
+	{#each ['+', '-', '×', '÷'] as handler}
+		<button class="bg-pink-700 text-white px-5 ms-5" on:click={() => addToExpression(handler)}
+			>{handler}</button
+		>
+	{/each}
+</div>
+
+<hr class="m-2" />
